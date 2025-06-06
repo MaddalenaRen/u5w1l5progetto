88f7a2d31a2e02de3605c37fdbcf2f475b01482c
MaddalenaRen <ileniarennella@gmail.com>
2025-06-06 10:38:05 +0200

    Initial commit

---
+++ src/main/java/it/epicode/u5w1l5progetto/U5w1l5progettoApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.u5w1l5progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w1l5progettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w1l5progettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/u5w1l5progetto/U5w1l5progettoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.u5w1l5progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w1l5progettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
